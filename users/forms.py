ef59a927cc66b4a448af4d441055779bab6cfeda
Marsel69 <zulpukarovmarsel30@gmail.com>
2022-11-30 17:43:08 +0600

    hw6

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    surname = forms.CharField()
     password = forms.CharField(min_length=3, max_length=20, widget=forms.PasswordInput)
-    password2 = forms.CharField(min_length=3, max_length=20, widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    password1 = forms.CharField(max_length=20, min_length=3, widget=forms.PasswordInput)
+    password2 = forms.CharField(max_length=20, min_length=3, widget=forms.PasswordInput)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from users.forms import LoginForm
+from users.forms import LoginForm, RegisterForm
+from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 # Create your views here.
 from users.utils import get_user_from_request
@@ -20,15 +21,13 @@ def login_view(request):
         if form.is_valid():
             user = authenticate(
                 username=form.cleaned_data.get('username'),
-                surname=form.cleaned_data.get('surname'),
                 password=form.cleaned_data.get('password'),
-                password2=form.cleaned_data.get('password2')
             )
             if user:
                 login(request, user)
                 return redirect('/posts')
             else:
-                form.add_error('плохой запрос')
+                form.add_error('password1', "errror")
 
         data = {
             'form': form
@@ -38,4 +37,31 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('/posts')
+
+
+def register_view(request):
+    if request.method == 'GET':
+        data = {
+            'form': RegisterForm,
+            'user': get_user_from_request(request)
+        }
+        return render(request, 'users/register.html', context=data)
+    elif request.method == 'POST':
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            if form.cleaned_data.get('password1') == form.cleaned_data.get('password2'):
+                user = User.objects.create_user(
+                    username=form.cleaned_data.get('username'),
+                    password=form.cleaned_data.get('password1')
+                )
+                login(request, user)
+                return redirect('/posts')
+            else:
+                form.add_error('password1', 'Пароль не совпадает !')
+
+        data = {
+            'form': form,
+            'user': get_user_from_request(request)
+        }
+        return render(request, 'users/register.html', context=data)
 
