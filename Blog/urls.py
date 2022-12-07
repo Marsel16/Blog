04a1e667297050cc74eb17fdfbe14ca10d5337d2
Marsel69 <zulpukarovmarsel30@gmail.com>
2022-12-07 17:27:32 +0600

    hw8

--- Blog/urls.py
+++ Blog/urls.py
@@ -19,13 +19,15 @@ from django.conf.urls.static import static
 from Blog import settings
 
 urlpatterns = [
+    path('', main_page_view),
     path('admin/', admin.site.urls),
-    path('hashtags/', hashtags_view),
-    path('posts/', posts_view),
-    path('posts/<int:id>/', detail_view),
-    path('posts/create/', posts_create_view),
+    path('hashtags/', HashtagsView.as_view()),
+    path('posts/', PostView.as_view()),
+    path('posts/<int:id>/', PostDetailView.as_view()),
+    path('posts/create/', PostCreateView.as_view()),
     path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- posts/views.py
+++ posts/views.py
@@ -3,15 +3,20 @@ from django.shortcuts import render, redirect
 from .models import Post, Hashtag, Comment
 from .forms import PostCreateForm, CommentCreateForm
 from users.utils import get_user_from_request
+from django.views.generic import ListView, CreateView, DetailView
 
 # Create your views here.
 PAGINATION_LIMIT = 3
 
-def posts_view(request):
-    if request.method == 'GET':
-        search_text = request.GET.get('search')
-        hashtag_id = request.GET.get('hashtag_id')
-        page = int(request.GET.get('page', 1))
+
+class PostView(ListView):
+    template_name = 'posts/posts.html'
+    queryset = Post.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        search_text = self.request.GET.get('search')
+        hashtag_id = self.request.GET.get('hashtag_id')
+        page = int(self.request.GET.get('page', 1))
         if hashtag_id:
             posts = Post.objects.filter(hashtag=Hashtag.objects.get(id=hashtag_id))
         else:
@@ -21,73 +26,65 @@ def posts_view(request):
         max_page = round(posts.__len__() / PAGINATION_LIMIT)
         page = int(page)
         posts = posts[PAGINATION_LIMIT * (page - 1): PAGINATION_LIMIT * page]
-        data = {
+        return {
             'posts': posts,
-            'user': get_user_from_request(request),
+            'user': get_user_from_request(self.request),
             'hashtag_id': hashtag_id,
             'current_page': page,
             "search_text": search_text,
             'max_page': list(range(1, max_page + 1))
         }
-        return render(request, 'posts/posts.html', context=data)
 
 
-def hashtags_view(request):
-    if request.method == 'GET':
-        hashtags = Hashtag.objects.all()
-        data = {
-            'hashtags': hashtags,
-            'user': get_user_from_request(request)
+class HashtagsView(ListView):
+    model = Hashtag
+    queryset = Hashtag.objects.all()
+    template_name = 'hashtags/hashtags.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        return {
+            'object_list': self.get_queryset(),
+            'user': get_user_from_request(self.request)
         }
-        return render(request, 'hashtags/hashtags.html', context=data)
 
 
-def detail_view(request, **kwargs):
-    if request.method == 'GET':
-        post = Post.objects.get(id=kwargs['id'])
+class PostDetailView(DetailView, CreateView):
+    template_name = 'posts/detail.html'
+    queryset = Post.objects.all()
+    form_class = PostCreateForm
+    pk_url_kwarg = 'id'
 
-        data = {
+    def get_context_data(self, *, object_list=None, **kwargs):
+        post = self.get_object()
+        return {
             'post': post,
             'comments': Comment.objects.filter(post=post),
             'form': CommentCreateForm,
-            'user': get_user_from_request(request)
+            'user': get_user_from_request(self.request)
         }
 
-        return render(request, 'posts/detail.html', context=data)
-
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         form = CommentCreateForm(data=request.POST)
 
         if form.is_valid():
             Comment.objects.create(
-                author_id=1,
+                author=request.user,
                 text=form.cleaned_data.get('text'),
-                post_id=kwargs['id']
+                post_id=self.get_object().id
             )
-            return redirect(f'/posts/{kwargs["id"]}/')
-        else:
-            post = Post.objects.get(id=kwargs['id'])
-            comments = Comment.objects.filter(post=post)
+            return redirect(f'/posts/{self.get_object().id}/')
+        return render(request, self.template_name, context=self.get_context_data(form=form))
 
-            data = {
-                'post': post,
-                'comments': comments,
-                'form': form,
-                'user': get_user_from_request(request)
-            }
-            return render(request, 'posts/detail.html', context=data)
-
-
-def posts_create_view(request):
-    if request.method == 'GET':
-        data = {
+class PostCreateView(CreateView):
+    form_class = PostCreateForm
+    template_name = 'posts/create.html'
+    def get_context_data(self, **kwargs):
+        return {
             'form': PostCreateForm,
-            'user': get_user_from_request(request)
+            'user': get_user_from_request(self.request)
         }
-        return render(request, 'posts/create.html', context=data)
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         form = PostCreateForm(data=request.POST)
-
         if form.is_valid():
             Post.objects.create(
                 title=form.cleaned_data.get('title'),
@@ -98,5 +95,8 @@ def posts_create_view(request):
             data = {
                 'form': form,
                 'user': get_user_from_request(request)
-            }
-            return render(request, 'posts/create.html', context=data)
+                }
+            return render(request, self.template_name,self.get_context_data(form=form))
+
+def main_page_view(request):
+    return render(request, 'main_page/main_page.html')
